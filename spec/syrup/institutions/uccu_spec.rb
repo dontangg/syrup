2a238bc78eb53d5a7ec0f5a1fbf3237f1be29457
Don Wilson <robert.don.wilson@gmail.com>
2011-10-08 10:31:41 -0600

    Can now fetch transactions from UCCU

--- spec/syrup/institutions/uccu_spec.rb
+++ spec/syrup/institutions/uccu_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 include Institutions
 
-describe Uccu do #, :bank_integration => true
+describe Uccu, :bank_integration => true do
   before(:each) do
     @bank = Uccu.new
     @bank.setup do |config|
@@ -21,21 +21,25 @@ describe Uccu do #, :bank_integration => true
   end
   
   it "fetches transactions given a date range" do
-    # account_id = 1    
-    #       
-    #     account = @bank.find_account_by_id(account_id)
-    #     account.instance_variable_get(:@prior_day_balance).should be_nil
-    #     account.instance_variable_get(:@current_balance).should be_nil
-    #     account.instance_variable_get(:@available_balance).should be_nil
-    #     
-    #     @bank.fetch_transactions(account_id, Date.today - 30, Date.today)
-    #     
-    #     puts account.prior_day_balance
-    #     puts account.current_balance
-    #     puts account.available_balance
-    #     
-    #     account.prior_day_balance.should_not be_nil
-    #     account.current_balance.should_not be_nil
-    #     account.available_balance.should_not be_nil
+    account_id = 1
+      
+    account = @bank.find_account_by_id(account_id)
+    account.instance_variable_get(:@prior_day_balance).should be_nil
+    account.instance_variable_get(:@current_balance).should be_nil
+    account.instance_variable_get(:@available_balance).should be_nil
+    
+    transactions = @bank.fetch_transactions(account_id, Date.today - 30, Date.today)
+    
+    transactions.each do |t|
+      p t
+    end
+    
+    puts account.prior_day_balance
+    puts account.current_balance
+    puts account.available_balance
+    
+    account.prior_day_balance.should be_nil
+    account.current_balance.should_not be_nil
+    account.available_balance.should_not be_nil
   end
 end
