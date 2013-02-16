21dfcec2ec1fc90eff84f88e4fbd3398d7615d46
Don Wilson <robert.don.wilson@gmail.com>
2013-02-16 09:28:03 -0700

    Update Zions Bank login method

--- lib/syrup/institutions/zions_bank.rb
+++ lib/syrup/institutions/zions_bank.rb
@@ -126,12 +126,12 @@ module Syrup
           # Enter the username
           page = agent.get('https://www.zionsbank.com')
           form = page.form('logonForm')
-          form.pm_fp = "version%3D1%26pm%5Ffpua%3Dmozilla%2F5%2E0%20%28windows%20nt%206%2E1%3B%20wow64%29%20applewebkit%2F535%2E19%20%28khtml%2C%20like%20gecko%29%20chrome%2F18%2E0%2E1025%2E162%20safari%2F535%2E19%7C5%2E0%20%28Windows%20NT%206%2E1%3B%20WOW64%29%20AppleWebKit%2F535%2E19%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F18%2E0%2E1025%2E162%20Safari%2F535%2E19%7CWin32%26pm%5Ffpsc%3D32%7C1920%7C1200%7C1200%26pm%5Ffpsw%3D%7Cqt1%7Cqt2%7Cqt3%7Cqt4%7Cqt5%7Cqt6%26pm%5Ffptz%3D%2D6%26pm%5Ffpln%3Dlang%3Den%2DUS%7Csyslang%3D%7Cuserlang%3D%26pm%5Ffpjv%3D1%26pm%5Ffpco%3D1"
-          form.publicCred1 = username
+          form.publicCred1 = self.username
+          form.privateCred1 = self.password
           page = form.submit
 
           # If the supplied username is incorrect, raise an exception
-          raise InformationMissingError, "Invalid username" if page.title == "Error Page"
+          raise InformationMissingError, "Invalid username/password" if page.title == "Error Page"
 
           # Go on to the next page
           page = page.links.first.click
@@ -141,34 +141,26 @@ module Syrup
             url = refresh[1]
             page = agent.get("https://securentry.zionsbank.com#{url}")
           end
-          
-          # Skip the secret question if we are prompted for the password
-          unless page.body.include?("Site Validation and Password")
-            # Find the secret question
-            question = page.search('div.form_field')[2].css('div').inner_text
-            
-            # If the answer to this question was not supplied, raise an exception
-            raise InformationMissingError, "Please answer the question, \"#{question}\"" unless secret_questions && secret_questions[question]
-            
-            # Enter the answer to the secret question
-            form = page.forms.first
-            form["challengeEntry.answerText"] = secret_questions[question]
-            form.radiobutton_with(:value => 'false').check
-            submit_button = form.button_with(:name => '_eventId_submit')
-            page = form.submit(submit_button)
-            
-            # If the supplied answer is incorrect, raise an exception
-            raise InformationMissingError, "\"#{secret_questions[question]}\" is not the correct answer to, \"#{question}\"" unless page.search('#errorComponent').empty?
-          end
 
-          # Enter the password
-          form = page.forms.first
-          form.privateCred1 = password
-          submit_button = form.button_with(:name => '_eventId_submit')
-          page = form.submit(submit_button)
-          
-          # If the supplied password is incorrect, raise an exception
-          raise InformationMissingError, "An invalid password was supplied" unless page.search('#errorComponent').empty?
+          # TODO: figure out how this is supposed to work now
+          # Skip the secret question if we are prompted for the password
+          #unless page.body.include?("Site Validation and Password")
+          #  # Find the secret question
+          #  question = page.search('div.form_field')[2].css('div').inner_text
+          #  
+          #  # If the answer to this question was not supplied, raise an exception
+          #  raise InformationMissingError, "Please answer the question, \"#{question}\"" unless secret_questions && secret_questions[question]
+          #  
+          #  # Enter the answer to the secret question
+          #  form = page.forms.first
+          #  form["challengeEntry.answerText"] = secret_questions[question]
+          #  form.radiobutton_with(:value => 'false').check
+          #  submit_button = form.button_with(:name => '_eventId_submit')
+          #  page = form.submit(submit_button)
+          #  
+          #  # If the supplied answer is incorrect, raise an exception
+          #  raise InformationMissingError, "\"#{secret_questions[question]}\" is not the correct answer to, \"#{question}\"" unless page.search('#errorComponent').empty?
+          #end
 
           # Clicking this link logs us into the banking.zionsbank.com domain
           page = page.links.first.click
